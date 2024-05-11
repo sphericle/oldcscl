b8307a351883ad22918b9a025c2925bb22bbebd0
zel <166771921+zealousgd@users.noreply.github.com>
2024-05-10 17:11:27 -0700

    Update score.js

--- js/score.js
+++ js/score.js
@@ -36,7 +36,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
