4de2f77ccb45c085e71dbd8642736c19f30be8c6
sphericle <86196223+sphericle@users.noreply.github.com>
2024-05-12 15:05:56 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -56,6 +56,9 @@ export function round(num) {
 export function average(num) {
 // add all scores together and divide by number of scores
 
+for (let i = 0; i < record.rating; i++) {
+  average += rating[i] + "<br>";
+}
 }
 
 
