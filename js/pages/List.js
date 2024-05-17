ca45ac7e415ba66fd143d54fdcf094d39d3aff99
sphericle <86196223+sphericle@users.noreply.github.com>
2024-05-16 22:39:41 -0400

    homepage

--- js/pages/List.js
+++ js/pages/List.js
@@ -37,7 +37,7 @@ export default {
                 </table>
             </div>
             <div class="level-container">
-                <div class="level" v-if="level">
+                <div class="level" v-if="level && level.id != 0">
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
