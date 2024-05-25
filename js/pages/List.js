95d73c991dd4f84c81f3d53679715854723e2fee
sphericle <86196223+sphericle@users.noreply.github.com>
2024-05-25 14:17:56 -0400

    rip..

--- js/pages/List.js
+++ js/pages/List.js
@@ -60,7 +60,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Song</div>
-                            <p v-if="level['song-link']"><a :href="../../data/nongs/level['song-link'].mp3" target="_blank"><u>{{ level['song-title'] }}</u></a></p>
+                            <p v-if="level['song-link']"><a :href="level['song-link']" target="_blank"><u>{{ level['song-title'] }}</u></a></p>
                             <p v-else>{{ level['song-title'] }}</p>
                         </li>
                     </ul>
