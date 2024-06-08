e23d8c4be39e010f173be23e5bfdde401816894c
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-08 15:32:54 -0400

    my bad

--- js/pages/Guidelines.js
+++ js/pages/Guidelines.js
@@ -14,7 +14,7 @@ export default {
                         If you have any questions, please join the <a href="https://discord.gg/87QtEZv5kp" target="_blank">Discord Server</a>.
                     </p>
                 </div>
-                <p><a @click="selected = 0)"><b>Home</b></a></p>
+                <p><a @click="selected = 0"><b>Home</b></a></p>
                 <p><a @click="selected = 1"><b>Submission Requirements</b></a></p>
                 <p><a @click="selected = 2"><b>Challenge Standards</b></a></p>
                 <p><a @click="selected = 3"><b>Challenge Standards</b></a></p>
