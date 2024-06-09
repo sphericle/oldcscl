8bf97c9af01869e156af2b2606472073c0888786
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-08 20:37:18 -0400

    mb..

--- js/pages/Guidelines.js
+++ js/pages/Guidelines.js
@@ -6,7 +6,7 @@ export default {
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
-        <main v-else class="page-guidelines">
+        <main v-else class="page-roulette">
             <div class="sidebar" style="margin: 15px;">
                 <div id="discord" style="text-align: left;">
                     <p class="type-label-md" style="color: #aaa">
