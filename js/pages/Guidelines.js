b67376bfb27afcac01795faa991c5e51d091b4b9
sphericle <86196223+sphericle@users.noreply.github.com>
2024-06-12 00:17:09 -0400

    Update Guidelines.js

--- js/pages/Guidelines.js
+++ js/pages/Guidelines.js
@@ -19,7 +19,6 @@ export default {
                 <p style="cursor:pointer"><a @click="selected = 2"><b>Challenge Standards</b></a></p>
                 <p style="cursor:pointer"><a @click="selected = 4"><b>Reliable System</b></a></p>
                 </div>
-                </div>
             <section style="margin:13px;" class="levels-container">
                 <div style="text-align: center;" class="levels" v-if="selected === 0">
                     <h1>Clicksync Challenge List Guidelines</h1>
