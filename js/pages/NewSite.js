04430164632cb0b305805a54d20668a85aa13485
sphericle <86196223+sphericle@users.noreply.github.com>
2024-07-10 16:42:29 -0400

    a

--- js/pages/NewSite.js
+++ js/pages/NewSite.js
@@ -8,11 +8,8 @@ export default {
         </main>
         <main v-else class="page-roulette" id="page-guidelines">
             <section id="all-docs" style="margin:13px;" class="levels-container">
-                <div style="text-align: center;" class="levels" v-if="selected === 0">
-                    <h1>Clicksync Challenge List Guidelines</h1>
-                    <h2>
-                        We've moved!
-                    </h2>
+                <div style="text-align: center;left: 0;line-height: 200px;margin-top: -100px;position: absolute;top: 50%;width: 100%;" class="levels" v-if="selected === 0">
+                    <h1>We've moved!</h1>
                     <p>
                         <b>This is the old CSCL website.</b> We've moved to a <a href="https://cscl.shuttleapp.rs/list/"><u>new, much cleaner</u></a> one as of July 10th, 2024. Check it out!  
                     </p>
