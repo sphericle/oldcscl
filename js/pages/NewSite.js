1a2c9d657f0fb113c0d7ddae96552131f7cf6715
sphericle <86196223+sphericle@users.noreply.github.com>
2024-07-10 16:47:52 -0400

    adsf

--- js/pages/NewSite.js
+++ js/pages/NewSite.js
@@ -9,8 +9,8 @@ export default {
         <main v-else class="page-roulette" id="page-guidelines">
             <section id="all-docs" style="margin:13px;" class="levels-container">
                 <div style="text-align: center;left: 0;line-height: 200px;margin-top: -100px;position: absolute;top: 50%;width: 100%;" class="levels" v-if="selected === 0">
-                    <h1>We've moved!</h1>
-                    <p>
+                    <h1 style="font-size:80px;margin-bottom:50px;">We've moved!</h1>
+                    <p style="font-size:30px">
                         <b>This is the old CSCL website.</b> We've moved to a <a href="https://cscl.shuttleapp.rs/list/"><u>new, much cleaner</u></a> one as of July 10th, 2024. Check it out!  
                     </p>
                 </div>
